5b46460368592c20677938a62c926f84a06b3321
Valentin Ageev <sazuru@gmail.com>
2020-02-27 22:27:23 +0300

    Add some news and ternary classes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
-import News from "./News";
+import News, { myNews } from "./News";
 import Comments from "./Comments";
 
 function App() {
   return (
     <>
-      <News />
+      <News data={myNews} />
       <Comments />
     </>
   );
--- src/News.js
+++ src/News.js
@@ -1,11 +1,53 @@
 import React from "react";
 
-function News() {
-  return (
-    <>
-      <p>К сожалению, новостей нет</p>
-    </>
-  );
+export const myNews = [
+  {
+    id: 1,
+    author: "Саша Печкин",
+    text: "В четверг, четвертого числа...",
+  },
+  {
+    id: 2,
+    author: "Просто Вася",
+    text: "Считаю, что $ должен стоить 35 рублей!",
+  },
+  {
+    id: 3,
+    author: "Max Frontend",
+    text: "Прошло 2 года с прошлых учебников, а $ так и не стоит 35",
+  },
+  {
+    id: 4,
+    author: "Гость",
+    text: "Бесплатно. Без смс, про реакт, заходи - https://maxpfrontend.ru",
+  },
+];
+
+class News extends React.Component {
+  render() {
+    const { data } = this.props;
+    let newsTemplate;
+
+    if (data.length) {
+      newsTemplate = data.map(function(item) {
+        return (
+          <div key={item.id}>
+            <p className="news__author">{item.author}:</p>
+            <p className="news__text">{item.text}</p>
+          </div>
+        );
+      });
+    } else {
+      newsTemplate = <p>К сожалению, новостей нет</p>;
+    }
+
+    return (
+      <div className="news">
+        {newsTemplate}
+        {data.length ? <strong>Всего новостей: {data.length}</strong> : null}
+      </div>
+    );
+  }
 }
 
 export default News;
